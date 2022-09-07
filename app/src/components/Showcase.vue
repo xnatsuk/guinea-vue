9163ff776d34a73aef60677c9ff4940b131067b4
xnatsuk <xnatsuk@gmail.com>
2022-09-06 21:50:02 -0300

    Add card display

--- app/src/components/Showcase.vue
+++ app/src/components/Showcase.vue
@@ -1,22 +1,68 @@
+<script setup lang="ts">
+import { VueperSlide, VueperSlides } from 'vueperslides'
+import type { IPet } from '@/services/types'
+import 'vueperslides/dist/vueperslides.css'
+
+const breakpoints = {
+  1024: {
+    slideRatio: 1,
+  },
+
+  740: {
+    slideRatio: 1.3,
+  },
+
+  580: {
+    slideRatio: 2,
+  },
+}
+
+const pets = ref <Array<IPet>>([])
+
+watch(query.get(), result =>
+  pets.value = result?.getPets,
+)
+</script>
+
 <template>
-  <div class="carousel w-full">
-    <div id="item1" class="carousel-item w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full">
-    </div>
-    <div id="item2" class="carousel-item w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full">
-    </div>
-    <div id="item3" class="carousel-item w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full">
-    </div>
-    <div id="item4" class="carousel-item w-full">
-      <img src="https://placeimg.com/800/200/arch" class="w-full">
-    </div>
-  </div>
-  <div class="flex justify-center w-full py-2 gap-2">
-    <a href="#item1" class="btn btn-xs">1</a>
-    <a href="#item2" class="btn btn-xs">2</a>
-    <a href="#item3" class="btn btn-xs">3</a>
-    <a href="#item4" class="btn btn-xs">4</a>
+  <div class="flex justify-center">
+    <VueperSlides
+      :dragging-distance="10"
+      :infinite="true"
+      :slide-ratio="1 / 2"
+      :breakpoints="breakpoints"
+      bullets-outside
+    >
+      <VueperSlide
+        v-for="(pet, i) in pets"
+        :key="i"
+        :slides="pet"
+      >
+        <template #content>
+          <PetCard
+            :name="pet.name"
+            :birthday="pet.birthday"
+            :gender="pet.gender"
+            :species="pet.species"
+            :nickname="pet.nickname"
+            :death-date="pet.deathDate"
+            :favorite-food="pet.favoriteFood"
+            :favorite-activity="pet.favoriteActivity"
+            :description="pet.description"
+            :photo="pet.photo"
+          />
+        </template>
+      </VueperSlide>
+    </VueperSlides>
   </div>
 </template>
+
+<style scoped>
+  .vueperslides {
+    width: 90%;
+  }
+
+  .vueperslide {
+    margin-top: 10%;
+  }
+</style>
