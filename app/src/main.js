66bf0f39ec81593beda539f00be6166a789b6ac1
xnatsuk <xnatsuk@gmail.com>
2022-09-13 16:50:42 -0300

    Format code

--- app/src/main.js
+++ app/src/main.js
@@ -1,10 +1,10 @@
 import { createApp, h, provide } from 'vue'
 import { DefaultApolloClient } from '@vue/apollo-composable'
 import { defaultConfig, plugin } from '@formkit/vue'
+import formKitConfig from '../formkit.config.js'
 import { apolloClient, apolloProvider } from './apollo-client'
 import App from './App.vue'
 import router from './router'
-import formKitConfig from '../formkit.config.js';
 import './assets/styles.css'
 
 const app = createApp({
