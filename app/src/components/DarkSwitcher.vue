e6cd2b55af134bb2ce316ded110b4712e7acc954
xnatsuk <xnatsuk@gmail.com>
2022-09-28 20:53:41 -0300

    fix(navigation): update router-link to use n-icon

--- app/src/components/DarkSwitcher.vue
+++ app/src/components/DarkSwitcher.vue
@@ -4,8 +4,8 @@
 <template>
   <n-button quaternary type="primary" size="large" @click="toggleDark()">
     <template #icon>
-      <n-icon v-if="!isDark" class="i-fluent-weather-moon-20-filled w-5 h-5" />
-      <n-icon v-else class="i-fluent-weather-sunny-20-filled w-5 h-5" />
+      <n-icon v-if="!isDark" class="i-fluent-weather-moon-20-filled " size="36" />
+      <n-icon v-else class="i-fluent-weather-sunny-20-filled" size="36" />
     </template>
   </n-button>
 </template>
--- app/src/components/Navigation.vue
+++ app/src/components/Navigation.vue
@@ -5,13 +5,19 @@
   <div class="flex justify-wrap m-x-4">
     <n-tabs type="line" size="large" class="flex justify-center m-l-12">
       <n-tab name="Home">
-        <router-link to="/" class="i-carbon-home w-8 h-8" />
+        <router-link to="/">
+          <n-icon class="i-carbon-home dark:bg-#63e2b7 bg-#18a058" size="36" />
+        </router-link>
       </n-tab>
       <n-tab name="EditPets">
-        <router-link to="/edit" class="i-carbon-edit w-8 h-8" />
+        <router-link to="/edit">
+          <n-icon class="i-carbon-edit dark:bg-#63e2b7 bg-#18a058" size="36" />
+        </router-link>
       </n-tab>
       <n-tab name="About">
-        <router-link to="/about" class="i-carbon-information w-8 h-8" />
+        <router-link to="/about">
+          <n-icon class="i-carbon-information dark:bg-#63e2b7 bg-#18a058" size="36" />
+        </router-link>
       </n-tab>
     </n-tabs>
     <div class="m-y-3">
@@ -19,17 +25,3 @@
     </div>
   </div>
 </template>
-
-<style scoped>
- .i-carbon-edit {
-    @apply dark:bg-#63e2b7 bg-#18a058;
-  }
-
-  .i-carbon-home {
-    @apply dark:bg-#63e2b7 bg-#18a058;
-  }
-
-  .i-carbon-information {
-    @apply dark:bg-#63e2b7 bg-#18a058;
-  }
-</style>
