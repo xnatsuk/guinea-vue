e95d4f74f75a2c241972c199c93c8596f288fcad
xnatsuk <xnatsuk@gmail.com>
2022-10-10 22:27:37 -0300

    fix(server): update types and resolvers to match schema
    createPet mutation now accepts a Pet object instead of only name

--- server/src/graphql/schema/index.ts
+++ server/src/graphql/schema/index.ts
@@ -33,13 +33,27 @@ const typeDefs = gql`
     photo: String
   }
 
+  input CreatePet {
+    _id: ID
+    name: String!
+    birthday: Date
+    gender: String
+    species: String
+    nickname: String
+    deathDate: Date
+    favoriteFood: String
+    favoriteActivity: String
+    description: String
+    photo: String
+  }
+
   type Query {
     getPets: [Pet!]!
     findPet(name: String!): Pet
   }
 
   type Mutation {
-    createPet(name: String!): Pet
+    createPet(createPet: CreatePet): Pet
     updatePet(name: String! updatePet: UpdatePet): Pet
     deletePet(name: String!): Pet
   }
